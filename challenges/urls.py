a42226738611eae6685c73cf6d35604ed0a008af
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2025-06-04 14:12:14 +0300

    combine monthly routes

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january", views.index)
+    # path("january", views.january),
+    # path("february", views.february),
+    path("<int:month>", views.monthly_challenge_number),
+    path("<str:month>", views.monthly_challenge)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,34 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
-# Create your views here.
-def index(request):
-    return HttpResponse("This works!")
+monthly_challenges = {
+    "january": "Eat no meat for the entire month",
+    "february": "Walk for atleast 20 minutes every day!",
+    "march": "Learn Django for atleast 20 minutes every day",
+    "april": "Walk for atleast 20 minutes every day!",
+    "may": "Learn Django for atleast 20 minutes every day",
+    "june": "Eat no meat for the entire month",
+    "july": "Walk for atleast 20 minutes every day!",
+    "august": "Learn Django for atleast 20 minutes every day",
+    "september": "Eat no meat for the entire month",
+    "october": "Learn Django for atleast 20 minutes every day",
+    "november": "Eat no meat for the entire month",
+    "december": "Learn Django for atleast 20 minutes every day"
+}
 
+# Create your views here.
+def january(request):
+    return HttpResponse("January works! Eat no meat for the entire month")
+
+def february(request):
+    return HttpResponse("February works!")
+
+def monthly_challenge_number(request, month):
+    pass
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This month is not supported")
