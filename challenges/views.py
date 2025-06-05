9ca416a8ec3d81acd7af5a3e3020533eb88c5852
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2025-06-05 11:33:53 +0300

    template changes

--- challenges/views.py
+++ challenges/views.py
@@ -6,16 +6,16 @@ from django.template.loader import render_to_string
 monthly_challenges = {
     "january": "Eat no meat for the entire month",
     "february": "Walk for atleast 20 minutes every day!",
-    "march": "Learn Django for atleast 20 minutes every day",
-    "april": "Walk for atleast 20 minutes every day!",
+    "march": "Learn React for atleast 20 minutes every day",
+    "april": "Walk for atleast 30 minutes every day!",
     "may": "Learn Django for atleast 20 minutes every day",
     "june": "Eat no meat for the entire month",
     "july": "Walk for atleast 20 minutes every day!",
     "august": "Learn Django for atleast 20 minutes every day",
-    "september": "Eat no meat for the entire month",
-    "october": "Learn Django for atleast 20 minutes every day",
+    "september": "Eat no fries for the entire month",
+    "october": "Learn FastAPI for atleast 20 minutes every day",
     "november": "Eat no meat for the entire month",
-    "december": "Learn Django for atleast 20 minutes every day"
+    "december": "Learn AWS for atleast 20 minutes every day"
 }
 
 # Create your views here.
@@ -39,8 +39,12 @@ def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
         # response_data = f"<h1>{challenge_text}</h1>"
-        response_data = render_to_string("challenges/challenge.html")
-        return HttpResponse(response_data)
+        # response_data = render_to_string("challenges/challenge.html")
+        # return HttpResponse(response_data)
+        return render(request, "challenges/challenge.html", {
+            'text': challenge_text,
+            'month': month.capitalize()
+        })
     except:
         return HttpResponseNotFound("<h1>This month is not supported</h1>")
 
@@ -52,8 +56,11 @@ def index(request):
         month_path = "/challenges/" + month
         list_items += f"<li><a href='{month_path}'>{month.capitalize()}</a></li>"
 
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    # response_data = f"<ul>{list_items}</ul>"
+    # return HttpResponse(response_data)
+    return render(request, "challenges/index.html", {
+        'months': months
+    })
 
 # def index(request):
 #     render_to_string()
